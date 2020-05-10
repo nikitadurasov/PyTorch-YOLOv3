77141c27f09032097e030ff2f7c944bbac1f8e47
nikitadurasov <yassnda@gmail.com>
2020-05-10 22:03:24 +0200

    adding possible configs right into the repo (and not generating them from scratch)

--- ECP_preprocessing.sh
+++ ECP_preprocessing.sh
@@ -5,9 +5,8 @@ python preprocess_ECP_dataset.py.
 mv images/* data/custom/images/
 mv labels/* data/custom/labels/
 
-mv train.txt data/custom/
-mv valid.txt data/custom/
-mv classes.names data/custom/
+rm train.txt 
+rm valid.txt 
 
 rm -r images
 rm -r labels
--- ECP_preprocessing_validation.sh
+++ ECP_preprocessing_validation.sh
@@ -5,7 +5,7 @@ python preprocess_ECP_dataset_validation.py.
 mv images/* data/custom/validation_images/
 mv labels/* data/custom/validation_labels/
 
-mv full_valid.txt data/custom/
+rm full_valid.txt
 
 rm -r images
 rm -r labels
