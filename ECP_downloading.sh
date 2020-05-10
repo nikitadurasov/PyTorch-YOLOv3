baaf64ac0d14a516bc99c67fb55e0d36c25d3c3a
nikitadurasov <yassnda@gmail.com>
2020-05-10 20:54:45 +0200

    adding data downloading script

---
+++ ECP_downloading.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_day_img_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_day_img_train.zip 
+wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_day_labels_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_day_labels_train.zip 
+
+wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_night_img_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_night_img_train.zip 
+wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_night_labels_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_night_labels_train.zip 
