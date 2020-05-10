6df55256373ad8ecf3d6851b6dd177c07183e5ce
nikitadurasov <yassnda@gmail.com>
2020-05-10 21:25:50 +0200

    fast fix of unzipping in downloading script and changing name of counting script

--- ECP_downloading.sh
+++ ECP_downloading.sh
@@ -6,4 +6,4 @@ wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_day_label
 wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_night_img_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_night_img_train.zip 
 wget --auth-no-challenge --user=$1 --password=$2 --output-document=ECP_night_labels_train.zip http://eurocity-dataset.tudelft.nl//eval/downloadFiles/downloadFile/detection?file=ecpdata%2FECP_night_labels_train.zip 
 
-unzip *.zip
+unzip "*.zip"
